190e6793a278e9022f8ecf95b3ccc6e3de1a62a0
gastons26 <gatis.lapsins@gmail.com>
2019-09-07 23:47:21 +0300

    s

--- src/config/ApiConfig.ts
+++ src/config/ApiConfig.ts
@@ -1,4 +1,4 @@
 export class ApiConfig {
-    static API_BASE_URL = 'http://mototrase.lv/api';
+    static API_BASE_URL = '/api';
     static APP_VERSION = '1.0.0';
 };
--- src/stores/Contact.store.ts
+++ src/stores/Contact.store.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 import {ContactModel} from "./models/ContactModel";
 
 export class ContactStore {
+
     public async postContact(data: ContactModel): Promise<void> {
         await axios.post(`${ApiConfig.API_BASE_URL}/contact/send`, data);
     }
+
 }
